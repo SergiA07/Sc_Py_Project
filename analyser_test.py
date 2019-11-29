13da3ae1f3f502bc41c13633133582cbd9b36e5c
Sergi Ayora <sergi_ayora@hotmail.com>
2019-11-29 12:29:27 +0100

    analyser_test

---
+++ analyser_test.py
@@ -0,0 +1,68 @@
+
+
+
+from os import listdir
+from os.path import isfile, join
+
+import codecs
+import json
+
+import pandas as pd
+import numpy as np
+import librosa
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans #https://musicinformationretrieval.com/kmeans.html
+from sklearn.neighbors import NearestNeighbors
+
+
+# ----------- #
+
+CORPUS_PATH = "/Users/Sergi/Documents/SuperCollider/Proyectos/Eloi/sons"
+
+
+
+# ----------- #
+
+
+def is_valid_sound(file):
+    isValidSound = isfile(file) and file.lower().endswith(('.wav', '.aiff'))
+    if(not isValidSound): print('invalid sound format for ', file, 'only wav or aiff')
+    return isValidSound
+
+def track_info_creator(track_name, folder_path = CORPUS_PATH):
+    """Create a dictionary with the metadata that we want to store for each sound clip."""
+    track_info = {}
+    track_info['track_id'] = track_name # name will be id
+    track_info['absolute_path'] = track_name
+    return track_info
+
+
+def tracks_info(sound_files_path):
+
+    # Make a Pandas DataFrame with the metadata of our sound collection and save it
+    tracks_paths = [join(sound_files_path, track) for track in listdir(sound_files_path)]
+    valid_tracks = [f for f in tracks_paths if is_valid_sound(f)]
+
+    # df =  pd.DataFrame([track_info_creator(tr, sound_files_path) for tr in valid_tracks])
+    # df.sort_values('track_id', inplace=True) #sort alphabetically
+    # return df, valid_tracks
+
+    tracks_info = [track_info_creator(tr, sound_files_path) for tr in valid_tracks]
+    return tracks_info, valid_tracks
+
+
+def mfcc_analize(track_path, fft_size=2048, hop_size=512, n_features=20):
+    audio, sr = librosa.load(track_path)
+    mfcc_data = librosa.feature.mfcc(y=audio, sr=sr, n_fft=fft_size, hop_length=hop_size, n_mfcc=n_features)
+    return mfcc_data
+
+
+tracks = tracks_info(CORPUS_PATH)
+
+[print(track) for track in tracks]
+
+# resultat = [mfcc_analize(track.absolute_path) for track in tracks]
+
+
+#print(resultat.shape)
