a15c1ec5e027feaaffa95cf265a3e5b8a33bbffc
Sergi Ayora <sergi_ayora@hotmail.com>
2019-12-01 02:51:11 +0100

    analisi afegit a diccionari

--- analyser_test.py
+++ analyser_test.py
@@ -1,6 +1,3 @@
-
-
-
 from os import listdir
 from os.path import isfile, join
 
@@ -18,64 +15,71 @@ from sklearn.neighbors import NearestNeighbors
 
 # ----------- #
 
-CORPUS_PATH = "/Users/Sergi/Documents/SuperCollider/Proyectos/Eloi/sons"
+#CORPUS_FOLDER_PATH = join(getcwd(),  "sounds")
 
-
+CORPUS_FOLDER_PATH = "/Users/Sergi/Desktop/carpeta sense nom/sounds/"
 
 # ----------- #
 
 
 def is_valid_sound(file):
-    isValidSound = isfile(file) and file.lower().endswith(('.wav', '.aiff'))
-    if(not isValidSound): print('invalid sound format for ', file, 'only wav or aiff')
-    return isValidSound
-
+    return isfile(file) and file.lower().endswith(('.wav', '.aiff'))
 
 
 def track_info_creator(track_name, folder_path):
-    # Create a dictionary with the metadata that we want to store for each sound clip."""
-    track_name = track_name.split("/")
-    track_name = track_name[-1]
 
     track_info = {}
+    track_name = track_name.split("/")
+    track_name = track_name[-1]
     track_info["track_id"] = track_name
     track_info["absolute_path"] = join(folder_path, track_name)
     return track_info
 
 
-
-
-
-
 def tracks_info(sound_files_path):
 
-    # Make a Pandas DataFrame with the metadata of our sound collection and save it
+    valid_tracks_fullpaths = []
     tracks_paths = [join(sound_files_path, track) for track in listdir(sound_files_path)]
 
+    for path in tracks_paths:
+        if is_valid_sound(path):
+            valid_tracks_fullpaths.append(path)
+        else:
+            print('invalid format for ', path, ' ,only wav or aiff')
 
-    valid_tracks = [f for f in tracks_paths if is_valid_sound(f)]
-
+    tracks_info = [track_info_creator(track, sound_files_path) for track in valid_tracks_fullpaths]
+    return tracks_info, valid_tracks_fullpaths
 
     # df =  pd.DataFrame([track_info_creator(tr, sound_files_path) for tr in valid_tracks])
     # df.sort_values('track_id', inplace=True) #sort alphabetically
     # return df, valid_tracks
 
-    tracks_info = [track_info_creator(track, sound_files_path) for track in valid_tracks]
-    return tracks_info, valid_tracks
-
 
 def mfcc_analize(track_path, fft_size=2048, hop_size=512, n_features=20):
+
     audio, sr = librosa.load(track_path)
     mfcc_data = librosa.feature.mfcc(y=audio, sr=sr, n_fft=fft_size, hop_length=hop_size, n_mfcc=n_features)
-    return mfcc_data
+    for index, track in enumerate(tracks):
+        if track["absolute_path"] == track_path:
+            print('añadido mfcc de:', track_path)
+            tracks[index]["mfcc"] = mfcc_data[0]
 
 
-tracks = tracks_info(CORPUS_PATH)[0] #aqui estava el problema. Tens que dir-li que de les dos infos que return tracks_info nomes vols la 1.
+def centroid_analize(track_path, fft_size=2048, hop_size=512):
 
-print(tracks)
+    audio, sr = librosa.load(track_path)
+    centroid_data = librosa.feature.spectral_centroid(y=audio, sr=sr, n_fft=fft_size, hop_length=hop_size)
+    for index, track in enumerate(tracks):
+        if track["absolute_path"] == track_path:
+            print('añadido centroide de:', track_path)
+            tracks[index]["centroid"] = centroid_data[0]
 
-print()
 
-resultat = [mfcc_analize(track["absolute_path"]) for track in tracks]
+tracks = tracks_info(CORPUS_FOLDER_PATH)[0] #aqui estava el problema. Tens que dir-li que de les dos infos que return tracks_info nomes vols la 1.
 
-print(resultat)
+[centroid_analize(track["absolute_path"]) for track in tracks]
+[mfcc_analize(track["absolute_path"]) for track in tracks]
+
+
+#print(tracks)
+[print(track["track_id"], track["centroid"], track["mfcc"]) for track in tracks]
