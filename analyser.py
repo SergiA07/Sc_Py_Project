01e31a34bee47f3014e48350a445b9cacfc5cf24
Lluis Suros <lluissuros@gmail.com>
2019-12-09 20:53:47 +0100

    refactor FeatureAnalyser, better naming

--- analyser.py
+++ analyser.py
@@ -1,6 +1,6 @@
 from os import listdir, getcwd
 from os.path import isfile, join
-
+from operator import itemgetter
 import codecs
 import json
 
@@ -24,68 +24,70 @@ class FeatureAnalyser:
     def __init__(self, corpus_path=CORPUS_FOLDER_PATH):
         # constructor
         self.corpus_path = corpus_path
-        self.dict = self.create_feature_dictionaries()
+        self.features_dict = self.compute_feature_dictionaries()
 
     def is_valid_sound(self, file):
         return isfile(file) and file.lower().endswith(('.wav', '.aiff'))
 
-    def get_valid_tracks(self, path):
+    def valid_tracks_fullpaths(self, path):
         valid_tracks_fullpaths = []
-        tracks_paths = [join(path, track) for track in listdir(path)]
+        tracks_fullpaths = [join(path, track) for track in listdir(path)]
 
-        for path in tracks_paths:
+        for path in tracks_fullpaths:
             if self.is_valid_sound(path):
                 valid_tracks_fullpaths.append(path)
             else:
                 print('invalid format for ', path, ' ,only wav or aiff')
-
         return valid_tracks_fullpaths
 
-    def add_to_dict(self, dict, track_path, features, time_pos):
-        for feature, time in zip(features, time_pos):
-            dict[feature] = {
+    def add_to_features_dict(self, feature_dict, track_path, feature_values, time_pos):
+        for feature_val, time in zip(feature_values, time_pos):
+            if feature_val in feature_dict:
+                print('repeated value for ', track_path)
+            feature_dict[feature_val] = {
                 "path": track_path,
                 "time_pos": time
             }
 
-    def create_feature_dictionaries(self):
+    def compute_feature_dictionaries(self):
+        config_analysis = {
+            # contain the config info to create each feature dictionary
+            "centroid": {
+                "fft_size": 2048,
+                "hop_size": 512
+            },
+            "flatness": {
+                "fft_size": 2048,
+                "hop_size": 512
+            }
+        }
+
         features_dict = {
             "centroid": {},
             "flatness": {},
         }
 
-        centroid_config = {
-            "fft_size": 2048,
-            "hop_size": 512
-        }
-
-        flatness_config = {
-            "fft_size": 2048,
-            "hop_size": 512
-        }
-
-        valid_tracks = self.get_valid_tracks(self.corpus_path)
+        valid_tracks = self.valid_tracks_fullpaths(self.corpus_path)
 
         for track_path in valid_tracks:
-
             audio, sr = librosa.load(track_path)
 
-            self.add_to_dict(
+            fft_size, hop_size = itemgetter(
+                "fft_size", "hop_size")(config_analysis["centroid"])
+            self.add_to_features_dict(
                 features_dict["centroid"],
                 track_path,
-                self.centroid_analize(
-                    audio, sr, centroid_config["fft_size"], centroid_config["hop_size"]),
-                self.get_frames_time(
-                    audio, sr, centroid_config["fft_size"], centroid_config["hop_size"])
+                self.centroid_analize(audio, sr, fft_size, hop_size),
+                self.get_frames_time(audio, sr, fft_size, hop_size)
             )
 
-            self.add_to_dict(
+            fft_size, hop_size = itemgetter(
+                "fft_size", "hop_size")(config_analysis["flatness"])
+            self.add_to_features_dict(
                 features_dict["flatness"],
                 track_path,
-                self.flatness_analize(
-                    audio, flatness_config["fft_size"], flatness_config["hop_size"]),
-                self.get_frames_time(
-                    audio, sr, flatness_config["fft_size"], flatness_config["hop_size"])
+                self.flatness_analize(audio, sr, fft_size, hop_size),
+                self.get_frames_time(audio, sr, fft_size, hop_size)
             )
 
         return features_dict
@@ -95,7 +97,7 @@ class FeatureAnalyser:
             y=audio, sr=sr, n_fft=fft_size, hop_length=hop_size)
         return centroid_data[0]
 
-    def flatness_analize(self, audio, fft_size=2048, hop_size=512):
+    def flatness_analize(self, audio, sr, fft_size=2048, hop_size=512):
         flatness_data = librosa.feature.spectral_flatness(
             y=audio, n_fft=fft_size, hop_length=hop_size)
         return flatness_data[0]
@@ -114,12 +116,9 @@ class FeatureAnalyser:
         return keys[index_closest_key], index_closest_key
 
     def get_closest_frame(self, dict_name, K):
-        dict = self.dict[dict_name]
-        keys = [*dict]
+        features_dict = self.features_dict[dict_name]
+        keys = [*features_dict]
         closest_frame, _ = self.closest(keys, K)
-        path = dict[closest_frame]["path"]
-        time_pos = dict[closest_frame]["time_pos"]
+        path = features_dict[closest_frame]["path"]
+        time_pos = features_dict[closest_frame]["time_pos"]
         return path, time_pos
-
-    #dict = create_feature_dictionaries(CORPUS_FOLDER_PATH)
-    #path, time_pos = get_closest_frame(dict["flatness"], 0.023456)
