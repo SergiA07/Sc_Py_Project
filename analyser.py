e5105f9683b1d18766198c4fbd530b401eac0056
Lluis Suros <lluissuros@gmail.com>
2020-01-09 19:04:41 +0100

    refactor code, and make sure time_pos refers to beggining of frame

--- analyser.py
+++ analyser.py
@@ -4,7 +4,8 @@ from os.path import isfile, join, normpath
 from operator import itemgetter
 import codecs
 import simplejson as json
-#import json
+import math
+# import json
 
 import pandas as pd
 import numpy as np
@@ -56,8 +57,8 @@ class FeatureAnalyser:
             }
 
     def compute_feature_dictionaries(self):
-        with open(CONFIG_ANALYSIS_PATH) as json_file:
-            config_analysis = json.load(json_file)
+        with open(CONFIG_ANALYSIS_PATH) as f:
+            config_analysis = json.load(f)
 
         features_dict = {}
         for feature_name in config_analysis.keys():
@@ -66,38 +67,22 @@ class FeatureAnalyser:
         valid_tracks, tracks_names = self.valid_tracks_fullpaths()
 
         for track_path, track_name in zip(valid_tracks, tracks_names):
-            for feature_name in config_analysis.keys():
+            for feature_name, feature_config_analisis in config_analysis.items():
                 feature_folder = join(FEATURES_FILEPATH, feature_name)
                 feature_analisis_json = join(
                     feature_folder, track_name + '.json')
-                # if precomupted
-                if os.path.exists(feature_analisis_json):
-                    with open(feature_analisis_json) as json_file:
-                        analisis = json.load(json_file)
-                        feature_analisis, time_pos = itemgetter(
-                            "feature_analisis", "time_pos")(analisis)
-                        print("carregat", track_name, feature_name)
-                else:
-                    # else compute
-                    audio, sr = librosa.load(track_path, mono=False)
-                    if len(audio.shape) > 1:
-                        audio = audio[0]  # només agafem el canal esquerra
-                    # Necessari per alguns arxius de audio, veure realment que fa
-                    audio = np.asfortranarray(audio)
-                    fft_size, hop_size = itemgetter(
-                        "fft_size", "hop_size")(config_analysis[feature_name])
-                    function = getattr(self, feature_name + '_analize')
-                    feature_analisis = function(audio, sr, fft_size, hop_size)
-                    time_pos = self.get_frames_time(
-                        audio, sr, fft_size, hop_size)
 
-                    if not os.path.exists(feature_folder):
-                        os.makedirs(feature_folder)
-
-                    with open(feature_analisis_json, 'w') as f:
-                        json.dump({'feature_analisis': feature_analisis.tolist(
-                        ), 'time_pos': time_pos.tolist()}, f)
+                # if not precomupted, compute feature:
+                if not os.path.exists(feature_analisis_json):
+                    self.create_feature_analisis_json(
+                        feature_name, feature_config_analisis, track_path, feature_analisis_json)
                     print("computat", track_name, feature_name)
+
+                # load analisis from precomputed json
+                with open(feature_analisis_json) as f:
+                    feature_analisis, time_pos = itemgetter(
+                        "feature_analisis", "time_pos")(json.load(f))
+                    print("carregat", track_name, feature_name)
 
                 self.add_to_features_dict(
                     features_dict[feature_name],
@@ -105,36 +90,65 @@ class FeatureAnalyser:
                     feature_analisis,
                     time_pos
                 )
-
         return features_dict
 
-    def centroid_analize(self, audio, sr, fft_size=2048, hop_size=512):
+    def create_feature_analisis_json(self, feature_name, feature_config_analisis, track_path, json_to_save_name):
+        def normalise_audio(audio, hop_size):
+            '''performs tranformations to obtain a correct audio to analise:
+            - only use left channel on stereo signals
+            - discard last samples if the don't fill a whole frame
+            '''
+            if len(audio.shape) > 1:
+                audio = audio[0]  # only left channel
+            audio = np.asfortranarray(audio)  # otherwise some errors
+            exact_size = math.floor(len(audio)/hop_size) * hop_size
+            audio = audio[:exact_size]  # remove last samples if needed
+            return audio
+
+        def get_frame_time_positions(feature_analisis_results, sr, hop_size=512):
+            '''returns an array containing the time position at the start of each frame'''
+            frames_indices = np.arange(len(feature_analisis_results))
+            frame_time_pos = librosa.frames_to_time(
+                frames_indices, sr, hop_size)
+            return frame_time_pos
+
+        fft_size, hop_size = itemgetter(
+            "fft_size", "hop_size")(feature_config_analisis)
+        audio, sr = librosa.load(track_path, mono=False)
+        audio = normalise_audio(audio, hop_size)
+
+        function = getattr(self, feature_name + '_analiser')
+        feature_analisis = function(audio, sr, fft_size, hop_size)
+        time_pos = get_frame_time_positions(feature_analisis, sr, hop_size)
+
+        feature_folder = join(FEATURES_FILEPATH, feature_name)
+        if not os.path.exists(feature_folder):
+            os.makedirs(feature_folder)
+        with open(json_to_save_name, 'w') as f:
+            json.dump({'feature_analisis': feature_analisis.tolist(
+            ), 'time_pos': time_pos.tolist()}, f)  # save as json
+
+    ''' ======= ANALISIS FUNCTIONS: they must follow the naming {my_analisis}_analiser =============='''
+
+    def centroid_analiser(self, audio, sr, fft_size=2048, hop_size=512):
         centroid_data = librosa.feature.spectral_centroid(
-            y=audio, sr=sr, n_fft=fft_size, hop_length=hop_size)
+            y=audio, sr=sr, n_fft=fft_size, hop_length=hop_size, center=False)
         return centroid_data[0]
 
-    def flatness_analize(self, audio, sr, fft_size=2048, hop_size=512):
+    def flatness_analiser(self, audio, sr, fft_size=2048, hop_size=512):
         flatness_data = librosa.feature.spectral_flatness(
-            y=audio, n_fft=fft_size, hop_length=hop_size)
+            y=audio, n_fft=fft_size, hop_length=hop_size, center=False)
         return flatness_data[0]
-
-    def get_frames_time(self, audio, sr=22050, fft_size=2048, hop_size=512):
-        frames = librosa.samples_to_frames(len(audio), hop_size, fft_size)
-        frames_indices = np.arange(frames)
-        frame_time_pos = librosa.frames_to_time(
-            frames_indices, sr, hop_size, fft_size)
-        # ens dona la posició temporal de la meitat de cada frame de 2048 samples
-        return frame_time_pos
-
-    def closest(self, keys, target_value):
-        keys = np.asarray(keys)
-        index_closest_key = (np.abs(keys - target_value)).argmin()
-        return keys[index_closest_key], index_closest_key
+    '''============================================================================'''
 
     def get_closest_frame(self, feature_name, target_value):
+        def closest(keys, target_value):
+            keys = np.asarray(keys)
+            index_closest_key = (np.abs(keys - target_value)).argmin()
+            return keys[index_closest_key], index_closest_key
         features_dict = self.features_dict[feature_name]
         keys = [*features_dict]
-        closest_frame, _ = self.closest(keys, target_value)
+        closest_frame, _ = closest(keys, target_value)
         path = features_dict[closest_frame]["path"]
         time_pos = features_dict[closest_frame]["time_pos"]
         return path, time_pos
