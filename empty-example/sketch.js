370ca9e586c45264d7aeac615bb2f8b36be2ff44
Sergi Ayora <sergi_ayora@hotmail.com>
2020-02-06 18:38:59 +0100

    loadJSON_in_progress

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,3 +1,4 @@
+/*
 let dictionary;
 
 function preload() {
@@ -7,17 +8,32 @@ function preload() {
    dictionary = loadJSON(url);
    console.log(dictionary);
 }
+*/
 
+let dictionary;
 
+function gotData(data) {
+    dictionary = data;
 
+}
 
 function setup() {
   createCanvas(400, 400)
-  createDiv(JSON.stringify(dictionary))
+  loadJSON('http://127.0.0.1:5002/analysis', gotData);
+  //createDiv(JSON.stringify(dictionary))
 }
 
 function draw() {
-    if (!dictionary) {
+    background(0);
+    if (dictionary) {
+        //randomSeed(4);
+        background(100);
+        textSize(10)
+        text(JSON.stringify(dictionary.centroid.by_values), random(10,300), random(10,300))
+    }
+
+    /*
+    if (!data) {
     // Wait until the earthquake data has loaded before drawing.
     background(0, 0, 0);
     return;
@@ -27,4 +43,12 @@ function draw() {
   background(200, 100, 300)
   textSize(10)
   text("fast", random(10,300), random(10,300))
+
+
+  background(0);
+  for (var i=0; i<data.centroid; i++) {
+      fill(225);
+      ellipse(random(width), random(height), 8, 8);
+  }
+  */
 }
