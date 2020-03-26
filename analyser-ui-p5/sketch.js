95a1fef8c83f8c85a8cc5250aca46b259b411f62
Sergi Ayora <sergi_ayora@hotmail.com>
2020-03-26 17:41:32 +0100

    start_sample, frame_dur values fixed

--- analyser-ui-p5/sketch.js
+++ analyser-ui-p5/sketch.js
@@ -2,47 +2,9 @@ let dictionary;
 let maxCentroid;
 let minCentroid;
 let numbersOnly;
+let featureCounter = 0;
 
 let centroid_data_points = [];
-
-function gotData(data) {
-  console.log("DATA!");
-  dictionary = data;
-  createDiv(JSON.stringify(dictionary.centroid.sample_rate));
-  createCentroidDataPoints(dictionary);
-
-  createDiv(numbersOnly);
-  createDiv(min);
-}
-
-function createCentroidDataPoints(dictionary) {
-  featureName = "centroid";
-  numbersOnly = Object.keys(dictionary[featureName].by_values);
-  minCentroid = min(numbersOnly);
-  maxCentroid = max(numbersOnly);
-  for (i = 0; i < numbersOnly.length; i++) {
-    data_value = numbersOnly[i];
-    let x = map(data_value, minCentroid, maxCentroid, 0, width);
-    let y = 100;
-    let r = 16;
-    let col = color(255, 0, 0, 10);
-    const { path, time_pos } = dictionary[featureName].by_values[data_value];
-    const frame_dur_hardcoded = 5000; // TODO
-    const feature_name_hardcoded = "centroid"; // TODO: right now is "centroid", but it could be anything
-    let data_for_sc = [
-      path,
-      time_pos,
-      frame_dur_hardcoded,
-      feature_name_hardcoded
-    ];
-    let sendDataToSC = () => {
-      console.log(data_for_sc);
-      sendOsc("/data", data_for_sc);
-    };
-    let c = new DataPoint(x, y, r, col, sendDataToSC);
-    centroid_data_points.push(c);
-  }
-}
 
 function setup() {
   button = createButton("test send OSC SC");
@@ -62,6 +24,52 @@ function draw() {
       centroid_data_points[i].show();
       centroid_data_points[i].onHovered(mouseX, mouseY);
     }
+  }
+}
+
+
+function gotData(data) {
+  console.log("DATA!");
+  dictionary = data;
+  const features = Object.keys(dictionary);
+  for(i=0; i < features.length; i++){
+      createDataPoints(dictionary, features[i]);
+      createDiv(features[i]);
+      featureCounter = featureCounter + 1;
+  }
+  //createDiv(JSON.stringify(dictionary.centroid.sample_rate));
+}
+
+function createDataPoints(dictionary, feature) {
+  createDiv(typeof feature);
+  featureName = feature;
+  numbersOnly = Object.keys(dictionary[featureName].by_values);
+  let feature_sampleRate = dictionary[featureName].sample_rate;
+  let feature_fftSize = dictionary[featureName].fft_size;
+  minValue = min(numbersOnly);
+  maxValue = max(numbersOnly);
+  for (i = 0; i < numbersOnly.length; i++) {
+    data_value = numbersOnly[i];
+    let x = map(data_value, minValue, maxValue, 0, width);
+    let y = 100 + (featureCounter * 200);
+    let r = 16;
+    let col = color(255, y, 0, 10);
+    let { path, time_pos } = dictionary[featureName].by_values[data_value];
+    let start_sample = time_pos * feature_sampleRate;
+    let frame_dur = feature_fftSize / feature_sampleRate;
+    let feature_name = featureName; // TODO: right now is "centroid", but it could be anything
+    let data_for_sc = [
+        path,
+        start_sample,
+        frame_dur,
+        feature_name
+    ];
+    let sendDataToSC = () => {
+      console.log(data_for_sc);
+      sendOsc("/data", data_for_sc);
+    };
+    let c = new DataPoint(x, y, r, col, sendDataToSC);
+    centroid_data_points.push(c);
   }
 }
 
